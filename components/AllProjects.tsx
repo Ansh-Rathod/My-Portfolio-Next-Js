78c0c0a169353fb1a7392ab7cb0cd1b8cd56dc82
Ansh-Rathod <anshrathod29@gmail.com>
2022-09-30 13:54:11 +0530

    fix: tailwind css fix for desktops

--- components/AllProjects.tsx
+++ components/AllProjects.tsx
@@ -6,24 +6,26 @@ function AllProjects() {
   return (
     <div
       id="projects"
-      className="bg-no-repeat border-t-[0.6px] border-slate-200 bg-fill bg-[url('/daniel-huniewicz-iQaFCjuMTfo-unsplash.jpg')] desktop:container"
+      className="bg-no-repeat border-t-[0.6px] border-slate-200 bg-fill bg-[url('/daniel-huniewicz-iQaFCjuMTfo-unsplash.jpg')] bg-cover"
     >
       <div className="backdrop-blur-[100px] bg-white/60 pb-24">
-        <div className="max-w-[1000px] m-auto p-2 tablet:max-w-[550px] ">
-          <div className="py-20 px-4">
-            <h1 className="font-poppins pb-1 text-3xl uppercase text-black text-center">
-              Completed Projects
-            </h1>
+        <div className="desktop:container">
+          <div className="max-w-[1000px] m-auto p-2 tablet:max-w-[550px] ">
+            <div className="py-20 px-4">
+              <h1 className="font-poppins pb-1 text-3xl uppercase text-black text-center">
+                Completed Projects
+              </h1>
 
-            <p className="font-proxima text-slate-900 text-[18px] text-center">
-              I have worked with multiple technologies as a developer to build
-              and maintain my projects.
-            </p>
-          </div>
-          <div className="grid grid-cols-2 gap-4 mobile:grid-cols-1 tablet:grid-cols-1 tablet:px-4 mobile:px-2">
-            {projects.map((project: any) => (
-              <ProjectComponent key={project.name} project={project} />
-            ))}
+              <p className="font-proxima text-slate-900 text-[18px] text-center">
+                I have worked with multiple technologies as a developer to build
+                and maintain my projects.
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mobile:grid-cols-1 tablet:grid-cols-1 tablet:px-4 mobile:px-2">
+              {projects.map((project: any) => (
+                <ProjectComponent key={project.name} project={project} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- components/intro.tsx
+++ components/intro.tsx
@@ -6,71 +6,78 @@ const Intro = () => (
   <div>
     <div
       id="home"
-      className="box-border  desktop:container bg-no-repeat 
+      className="box-border  bg-no-repeat 
       border-t-[0.6px] border-slate-200
       bg-cover bg-[url('/daniel-huniewicz-iQaFCjuMTfo-unsplash.jpg')]"
     >
-      <div className="flex backdrop-blur-3xl bg-white/70 ">
-        <div
-          className="w-[55%] flex flex-col justify-between h-screen max-h-[808px]
+      <div className=" backdrop-blur-3xl bg-white/70  ">
+        <div className="desktop:max-w-[1800px] m-auto  flex">
+          <div
+            className="w-[55%] flex flex-col justify-between h-screen max-h-[808px]
           tablet:w-full tablet:max-w-[500px] tablet:m-auto
           mobile:w-full
         "
-        >
-          <div className="py-4 ">
-            <ul className="flex flex-row justify-between px-14 tablet:px-8 mobile:px-4">
-              <NavbarItem name="home" />
-              <NavbarItem name="About" />
-              <NavbarItem name="skills" />
-              <NavbarItem name="projects" />
-            </ul>
+          >
+            <div className="py-4 ">
+              <ul className="flex flex-row justify-between px-14 tablet:px-8 mobile:px-4">
+                <NavbarItem name="home" />
+                <NavbarItem name="About" />
+                <NavbarItem name="skills" />
+                <NavbarItem name="projects" />
+              </ul>
+            </div>
+            <div className="px-16 tablet:px-10 mobile:px-8">
+              <p className="font-proxima text-xl leading-10 mobile:text-[18px] mobile:py-2 ">
+                HELLO👋, I AM
+              </p>
+              <b className=" font-bold font-poppins text-5xl  leading-snug mini-laptop:text-4xl mini-laptop:leading-normal tablet:text-4xl tablet:leading-sung mobile:leading-[1.1]">
+                ANSH RATHOD
+              </b>
+              <p className="font-proxima text-[22px] text-slate-800 mini-laptop:text-[20px] tablet:text-[18px] mobile:text-[18px] mobile:py-2">
+                A full-stack developer, Building Seamless mobile & web
+                applications.
+              </p>
+            </div>
+            <div className="px-14 tablet:px-10 py-12 flex flex-row mobile:px-8">
+              <Icon
+                url="https://www.instagram.com/anshrathodd"
+                name="Instagram"
+                classes="fa-brands fa-instagram"
+              />
+              <Icon
+                url="https://github.in/Ansh-Rathod"
+                name="Github"
+                classes="fa-brands fa-github"
+              />
+              <Icon
+                url="https://twitter.com/AnshhRathod"
+                classes="fa-brands fa-twitter"
+                name="Twitter"
+              />
+              <Icon
+                url="https://www.linkedin.com/in/ansh-rathod-478a81210/"
+                classes="fa-brands fa-linkedin"
+                name="Linkedin"
+              />
+              <Icon
+                url="mailto:anshrathod29@gmail.com"
+                classes="fa-solid fa-envelope"
+                name="Email"
+              />
+            </div>
           </div>
-          <div className="px-16 tablet:px-10 mobile:px-8">
-            <p className="font-proxima text-xl leading-10 mobile:text-[18px] mobile:py-2 ">
-              HELLO👋, I AM
-            </p>
-            <b className=" font-bold font-poppins text-5xl  leading-snug mini-laptop:text-4xl mini-laptop:leading-normal tablet:text-4xl tablet:leading-sung mobile:leading-[1.1]">
-              ANSH RATHOD
-            </b>
-            <p className="font-proxima text-[22px] text-slate-800 mini-laptop:text-[20px] tablet:text-[18px] mobile:text-[18px] mobile:py-2">
-              A full-stack developer, Building Seamless mobile & web
-              applications.
-            </p>
-          </div>
-          <div className="px-14 tablet:px-10 py-12 flex flex-row mobile:px-8">
-            <Icon
-              url="https://www.instagram.com/anshrathodd"
-              classes="fa-brands fa-instagram"
-            />
-            <Icon
-              url="https://github.in/Ansh-Rathod"
-              classes="fa-brands fa-github"
-            />
-            <Icon
-              url="https://twitter.com/AnshhRathod"
-              classes="fa-brands fa-twitter"
-            />
-            <Icon
-              url="https://www.linkedin.com/in/ansh-rathod-478a81210/"
-              classes="fa-brands fa-linkedin"
-            />
-            <Icon
-              url="mailto:anshrathod29@gmail.com"
-              classes="fa-solid fa-envelope"
-            />
-          </div>
-        </div>
-        <div className="w-[45%] h-screen max-h-[808px] tablet:hidden mobile:hidden ">
-          <div className=" w-full h-full relative">
-            <Image
-              src="/intro (1123 × 1123 px).png"
-              layout="fill"
-              objectFit="cover"
-              className="object-left h-screen"
-              alt="intro"
-              placeholder="blur"
-              blurDataURL="/intro (1123 × 1123 px).png"
-            />
+          <div className="w-[45%] h-screen max-h-[808px] tablet:hidden mobile:hidden ">
+            <div className=" w-full h-full relative">
+              <Image
+                src="/intro (1123 × 1123 px).png"
+                layout="fill"
+                objectFit="cover"
+                className="object-left h-screen "
+                alt="intro"
+                placeholder="blur"
+                blurDataURL="/intro (1123 × 1123 px).png"
+              />
+            </div>
           </div>
         </div>
       </div>
