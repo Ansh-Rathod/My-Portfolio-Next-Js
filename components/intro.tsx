73292c872a06015b70afd94961b67a010b9b4922
Ansh Rathod <anshrathod29@gmail.com>
2022-12-27 17:04:26 +0530

    [feature] added new animations with framer motion

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 import NavbarItem from "../components/NavbarItem";
+import { motion } from "framer-motion";
 import Icon from "../components/Icon";
+
+const textAnimate = {
+  offscreen: { y: 50, opacity: 0 },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    transition: { type: "spring", bounce: 0.4, duration: 1 },
+  },
+};
 
 const Intro = () => (
   <div>
@@ -8,7 +18,7 @@ const Intro = () => (
       id="home"
       className="box-border  bg-no-repeat 
       border-t-[0.6px] border-slate-200
-      bg-cover bg-[url('/daniel-huniewicz-iQaFCjuMTfo-unsplash.jpg')]"
+      bg-cover bg-[url('/daniel-huniewicz-iQaFCjuMTfo-unsplash.webp')]"
     >
       <div className=" backdrop-blur-3xl bg-white/70  ">
         <div className="desktop:max-w-[1800px] m-auto  flex">
@@ -26,50 +36,70 @@ const Intro = () => (
                 <NavbarItem name="projects" />
               </ul>
             </div>
-            <div className="px-16 tablet:px-10 mobile:px-8">
-              <p className="font-proxima text-xl leading-10 mobile:text-[18px] mobile:py-2 ">
+            <motion.div
+              initial={"offscreen"}
+              whileInView={"onscreen"}
+              viewport={{ once: false, amount: 0.5 }}
+              transition={{ staggerChildren: 0.5 }}
+              className="px-16 tablet:px-10 mobile:px-8"
+            >
+              <motion.p
+                variants={textAnimate}
+                className="font-proxima text-xl leading-10 mobile:text-[18px] mobile:py-2 "
+              >
                 HELLO👋, I AM
-              </p>
-              <b className=" font-bold font-poppins text-5xl  leading-snug mini-laptop:text-4xl mini-laptop:leading-normal tablet:text-4xl tablet:leading-sung mobile:leading-[1.1]">
+              </motion.p>
+              <h1 className=" font-bold font-poppins text-5xl  leading-snug mini-laptop:text-4xl mini-laptop:leading-normal tablet:text-4xl tablet:leading-sung mobile:leading-[1.1]">
                 ANSH RATHOD
-              </b>
-              <p className="font-proxima text-[22px] text-slate-800 mini-laptop:text-[20px] tablet:text-[18px] mobile:text-[18px] mobile:py-2">
+              </h1>
+              <p className="font-proxima text-[22px] max-w-[600px] text-slate-800 mini-laptop:text-[20px] tablet:text-[18px] mobile:text-[18px] mobile:py-2">
                 A full-stack developer, Building Seamless mobile & web
                 applications.
               </p>
-            </div>
-            <div className="px-14 tablet:px-10 py-12 flex flex-row mobile:px-8">
+            </motion.div>
+            <motion.div
+              initial={{ y: 10, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+              viewport={{ once: false, amount: 0.6 }}
+              className="px-14 tablet:px-10 py-12 flex flex-row mobile:px-8"
+            >
               <Icon
+                i={1}
                 url="https://github.in/Ansh-Rathod"
                 name="Github"
                 classes="fa-brands fa-github"
               />
 
               <Icon
+                i={2}
                 url="https://twitter.com/AnshhRathod"
                 classes="fa-brands fa-twitter"
                 name="Twitter"
               />
               <Icon
+                i={3}
                 url="https://www.linkedin.com/in/ansh-rathod-478a81210/"
                 classes="fa-brands fa-linkedin"
                 name="Linkedin"
               />
               <Icon
+                i={4}
                 url="https://discordapp.com/users/768376622243119114"
                 classes="fa-brands fa-discord"
               />
               <Icon
+                i={5}
                 url="mailto:anshrathod29@gmail.com"
                 classes="fa-solid fa-envelope"
                 name="Email"
               />
               <Icon
+                i={6}
                 url="https://www.instagram.com/anshrathodd"
                 name="Instagram"
                 classes="fa-brands fa-instagram"
               />
-            </div>
+            </motion.div>
           </div>
           <div className="w-[45%] h-screen max-h-[808px] tablet:hidden mobile:hidden ">
             <div className=" w-full h-full relative">
