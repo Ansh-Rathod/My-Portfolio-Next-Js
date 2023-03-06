ba980056ec42202d7069033e9cf4a51e64e997e5
Ansh rathod <anshrathod29@gmail.com>
2023-03-06 17:26:05 +0530

    updated meme page

--- pages/memes.tsx
+++ pages/memes.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Memes() {
   return (
     <div className="flex flex-col w-full h-screen">
-      <p>Coming soon</p>
+      <p className="font-bold text-[20px] mb-4">Coming soon</p>
       <p>
         Follow{" "}
         <a className="text-blue-400" href="https://twitter.com/@anshrathodfr">
