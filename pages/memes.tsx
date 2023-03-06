ddd96ba7cf5708c0074f24706b545e744f925afb
Ansh rathod <anshrathod29@gmail.com>
2023-03-06 17:25:48 +0530

    added mems page

---
+++ pages/memes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Memes() {
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <p>Coming soon</p>
+      <p>
+        Follow{" "}
+        <a className="text-blue-400" href="https://twitter.com/@anshrathodfr">
+          @anshrathodfr
+        </a>{" "}
+        on twitter for the programming memes created by me.
+      </p>
+    </div>
+  );
+}
+
+export default Memes;
