cc3da15acf6d86b6fd05bc605c6972c6bb439a9d
Ansh rathod <anshrathod29@gmail.com>
2023-03-06 17:27:24 +0530

    updated meme page

--- pages/memes.tsx
+++ pages/memes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Memes() {
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="flex flex-col w-full h-screen text-center justify-center items-center">
       <p className="font-bold text-[20px] mb-4">Coming soon</p>
       <p>
         Follow{" "}
