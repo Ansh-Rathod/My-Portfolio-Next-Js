2c4e3f76ace7f00dba862c34e73ea6c1e3f2f42b
Ansh Rathod <anshrathod29@gmail.com>
2024-10-30 12:54:15 +0530

    change of plans

--- pages/switching/index.tsx
+++ pages/switching/index.tsx
@@ -13,15 +13,15 @@ export default function Component() {
         <meta name="twitter:creator" content="@anshrathodfr" />
         <meta
           name="twitter:title"
-          content="We're forking React native. This is why"
+          content="I'm switching to react native. This is why"
         />
         <meta
           property="og:title"
-          content="We're forking React native. This is why"
+          content="I'm switching to react native. This is why"
         />
         <meta
           property="og:site_name"
-          content="We're forking React native. This is why"
+          content="I'm switching to react native. This is why"
         />
         <meta
           name="twitter:image"
@@ -38,7 +38,8 @@ export default function Component() {
         <div className="h-60"></div>
         <article className="max-w-3xl mx-auto space-y-8">
           <h1 className="text-4xl md:text-5xl font-bold leading-tight tracking-tight">
-            We&apos;re forking React native. This is why.
+            After 4 years of building in flutter I&apos;m switching to react
+            native. This is why.
           </h1>
 
           <div className="flex items-center gap-3">
@@ -62,25 +63,28 @@ export default function Component() {
             <p>
               Over the years, There are many debates or Beginners questioning on
               whether React Native is best or flutter? I am here to answer none
-              of your questions. Go touch grass you computer nerd, cut down the
-              internet for a day and eat ice creem it&apos;s better than posting
-              memes on twitter and disucssing in comments. and about the
-              framework just use flutter if google kills flutter one day who
-              cares! we all can laugh at each other make more memes to share on
-              twitter and we all can go back to writing html css. in the end,
-              having fun is all that matters.
+              of your questions. it was clickbait to get you here, Go touch
+              grass you computer nerd, cut down the internet for a day and eat
+              ice creem it&apos;s better than posting memes on twitter thats
+              what my mom said. and about the switching framework i just use
+              flutter why would i spend more time learning new framework, jsx
+              syntex are bad and has horrible dev exprience with typescript type
+              errors eww, if google kills flutter one day i dont care! we all
+              will laugh and will make more memes to share on twitter and we all
+              can go back to writing html css. in the end, having fun is all
+              that matters.
             </p>
 
             <h2 className="text-3xl font-bold mt-12 mb-6">
-              React Native&apos;s not shortage of labour
+              React Native&apos;s is the best (maybe)
             </h2>
 
             <p>
-              if you wanna build a app chose react native, if you wanna have fun
-              chose flutter, if you wanna make money lets share memes on
+              if you wanna build a app choose react native, if you wanna have
+              fun cho0se flutter, if you wanna make money lets share memes on
               twitter, i chose twitter that why im writing and yeah i built
-              desktop app using flutter(i dont care tech stack you shouldnt too)
-              called{" "}
+              desktop app using flutter(i dont care about tech stack you
+              shouldnt too) called{" "}
               <Link href={"https://pimosa.app"} className="text-blue-500">
                 <span className="text-blue-400 cursor-pointer">
                   Pimosa it&apos;s all in one media tool kit buy now it is
@@ -88,7 +92,12 @@ export default function Component() {
                 </span>
               </Link>{" "}
               if flutter dies i dont care I&apos;m learning game dev, building
-              UI is boring now after doing it for the 4 years.
+              UI is boring now after doing it for the 4 years. My belief is that
+              make money by giving value to users and have fun, generally i like
+              building so i dont care about the tech stack, i just want to build
+              something that will be useful to users. dont get attached to these
+              frameworks its just zeros and onezies, 2030 nope i wont be using
+              it, or will i? maybe not maybe
             </p>
 
             <p>
