f7f4e0612dda29ad868f8dc98b331c091acbb321
Ansh Rathod <anshrathod29@gmail.com>
2025-09-06 15:03:34 +0530

    update

--- pages/fonts.ts
+++ pages/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 // Configure SpaceMono as the only font
-export default localFont({
+export const spaceMono = localFont({
   src: "../public/fonts/SpaceMono-Regular.ttf",
   variable: "--font-space-mono",
   display: "swap",
